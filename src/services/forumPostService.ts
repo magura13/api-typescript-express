4c46106e466a1938611c3eda250206f17d50db4f
Samuel Maia Salvador de Assis <samuelufop121035@gmail.com>
2024-01-23 15:52:06 -0300

    Adding "has more" propertie when gettint forumposts

--- src/services/forumPostService.ts
+++ src/services/forumPostService.ts
@@ -10,7 +10,10 @@ export class ForumPostService {
   public async getForumPosts(limit:number,offset:number) :Promise<Object> {
     const model = await ForumPostModel.getInstance();
     const data = await model.find({}).sort({'createdAt':1}).skip(offset).limit(limit).exec();
-    return data
+    const totalForumPosts = await model.countDocuments();
+    const hasMore = offset + limit < totalForumPosts
+    const response = {hasMore,data}
+    return response
   }
 
   public getforumPostbyId = async (forumPostId: string) : Promise<Object | null> => {
