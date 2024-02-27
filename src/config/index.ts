e17a0ef117525658c2ddd31cbf1db5f52247fbb6
magura13 <tiagolelis_@hotmail.com>
2024-02-27 18:30:15 -0300

    123

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,3 +1,9 @@
+import { config as loadConfig } from 'dotenv';
+
+loadConfig({
+    path: '.env',
+});
+
 interface AwsConfig {
     AccessKeyId: string | undefined;
     AWSSecretKey: string | undefined;
@@ -11,12 +17,12 @@ interface Config {
 }
 
 const config: Config = {
-    PORT: parseInt(process.env.PORT || '3010', 10),
+    PORT: parseInt(process.env.PORT || '', 10) || 3010,
     AWS: {
         AccessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        AWSSecretKey: process.env.AWS_SECRET_ACCESS_KEY,
+        AWSSecretKey: process.env.AWS_SECRET_KEY,
         BucketName: process.env.AWS_S3_BUCKET_NAME,
-        Region: process.env.AWS_REGION || 'us-east-1',
+        Region: 'us-east-1',
     },
 };
 
